6c74775c713b86c2af7751a4499094fc7e74f1a9
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-06-17 17:02:02 +0500

    Attach

--- src/test/java/owner/BrowserstackKeys.java
+++ src/test/java/owner/BrowserstackKeys.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:properties/browserstacktackDriver.properties"
+        "classpath:properties/browserstackDriver.properties"
 })
 public interface BrowserstackKeys extends Config {
     String username();
