10d29cc0464d5bc814cc494aca72476fc2b7c2ea
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-06-17 17:12:37 +0500

    change name

--- src/test/java/tests/MobileTests.java
+++ src/test/java/tests/MobileTests.java
@@ -5,17 +5,22 @@ import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 @Tag("android")
 public class MobileTests extends TestBase {
     @Test
     void searchTest() {
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+        back();
+        step("Search BrowserStack in Wikipedia", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
 
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        step("Verify results", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
+        });
 
-        $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(CollectionCondition.sizeGreaterThan(0));
     }
 }
