bc3db0ab128c2f46130062e0a5a143a450a0ea70
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-06-18 18:51:41 +0500

    Add new test

--- src/test/java/tests/MobileTests.java
+++ src/test/java/tests/MobileTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -28,7 +29,20 @@ public class MobileTests extends TestBase {
     }
 
     @Test
-    void openContent(){
+    @DisplayName("Open content")
+    void openContent() {
         back();
+        step("Search Beatles in Wikipedia", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_container")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Beatles");
+        });
+
+        step("Open content", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+        });
+
+        step("Verify title", () -> {
+            $(AppiumBy.className("android.widget.TextView")).shouldHave(Condition.text("The Beatles"));
+        });
     }
 }
