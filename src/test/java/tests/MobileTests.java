0bce37e9875e28356e0cd993cce13888cabb37d8
DmitriyDmitrishin <dmitrishin52@gmail.com>
2022-06-18 18:52:20 +0500

    Change name

--- src/test/java/tests/MobileTests.java
+++ src/test/java/tests/MobileTests.java
@@ -29,7 +29,7 @@ public class MobileTests extends TestBase {
     }
 
     @Test
-    @DisplayName("Open content")
+    @DisplayName("Check content")
     void openContent() {
         back();
         step("Search Beatles in Wikipedia", () -> {
